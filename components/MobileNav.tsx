4f8cddf6691de01017dde6f291add2a6e2cff73c
Kelvin Mutua <kelvinndomamutua@gmail.com>
2024-04-09 21:38:44 +0300

    created the sider

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -26,24 +26,24 @@ const MobileNav = () => {
         <SheetContent side="left" className="border-none bg-dark-1">
           <Link href="/" className="flex items-center gap-1">
             <Image
-              src="/icons/logo.svg"
+              src="/icons/play.svg"
               width={32}
               height={32}
               alt="yoom logo"
             />
-            <p className="text-[26px] font-extrabold text-white">YOOM</p>
+            <p className="text-[26px] font-extrabold text-white">Kel</p>
           </Link>
           <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
             <SheetClose asChild>
               <section className=" flex h-full flex-col gap-6 pt-16 text-white">
-                {sidebarLinks.map((item) => {
-                  const isActive = pathname === item.route;
+                {sidebarLinks.map((link) => {
+                  const isActive = pathname === link.route;
 
                   return (
-                    <SheetClose asChild key={item.route}>
+                    <SheetClose asChild key={link.route}>
                       <Link
-                        href={item.route}
-                        key={item.label}
+                        href={link.route}
+                        key={link.label}
                         className={cn(
                           'flex gap-4 items-center p-4 rounded-lg w-full max-w-60',
                           {
@@ -52,12 +52,12 @@ const MobileNav = () => {
                         )}
                       >
                         <Image
-                          src={item.imgUrl}
-                          alt={item.label}
+                          src={link.imgUrl}
+                          alt={link.label}
                           width={20}
                           height={20}
                         />
-                        <p className="font-semibold">{item.label}</p>
+                        <p className="font-semibold">{link.label}</p>
                       </Link>
                     </SheetClose>
                   );
